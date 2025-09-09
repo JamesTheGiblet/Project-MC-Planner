bc2715e5bb7136d903c70bea66c8276ae654cbba
JamesTheGiblet <gilbertjam@live.com>
2025-09-09 19:59:59 +0100

    Adjust pin layout gap for improved spacing in board data

--- data/rulebook.js
+++ data/rulebook.js
@@ -46,7 +46,7 @@ const boardData = {
         image: 'images/Raspberry Pi 4B.png',
         width: 1600,
         height: 967,
-        pinLayout: { top: '7%', right: '16.25%', gap: '5px 13.75%' },
+        pinLayout: { top: '7%', right: '16.25%', gap: '5px 6.9%' },
         layout: '2col-grid',
         pins: [
             { name: '3.3V', type: 'power', title: 'Pin 1: 3.3V Power Rail' },
