4c22578227f0d1b126574b98353b4aee51e9acb8
JamesTheGiblet <gilbertjam@live.com>
2025-09-12 16:55:49 +0100

    Adjust top margin in pin layout for improved design consistency

--- data/boards.js
+++ data/boards.js
@@ -7,7 +7,7 @@ const boardData = {
         width: 2100,
         height: 1270,
         pinLayout: {
-            top: '9%', right: '30%', gap: '1% 6.9%' },
+            top: '8%', right: '30%', gap: '1% 6.9%' },
         layout: '2col-grid',
         pins: [
             { name: '3.3V', type: 'power', title: 'Pin 1: 3.3V Power Rail' },
